7230ee542cc95b5267e7bee0615707d54567ad0c
AleDavMart <aleyda.davalos@gmail.com>
2022-06-05 21:39:38 -0700

    added a user model

--- server/models/db.js
+++ server/models/db.js
@@ -1,12 +1,34 @@
-const { Sequelize } = require('sequelize');
-
+const { Sequelize, DataTypes, Model } = require('sequelize');
 const sequelize = new Sequelize('postgresql://localhost/PsychichGuacamole', {
   logging: (...msg) => console.log(msg) //Displays all log function call parameters
 }); // passing a connection URI
 
+class User extends Model {}
+
+User.init({
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  userName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+},{
+  sequelize,
+  modelName: 'User'
+});
+
+
 try { // checking the connection to the db
   sequelize.authenticate();
-   console.log('Connection has been established successfully.');
- } catch (error) {
-   console.error('Unable to connect to the database:', error);
- }
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+
