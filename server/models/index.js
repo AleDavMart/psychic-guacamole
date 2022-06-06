a65488ca34f6dda7703742a02568adc6f35af303
AleDavMart <aleyda.davalos@gmail.com>
2022-06-06 08:24:50 -0700

    created association file

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const { Sequelize, DataTypes, Model } = require('./db');
+
+const { User } = require('./User')
+const {Product} = require('./Product')
+const {Cart} = require('./Cart')
+const {Payment} = require ('./Payment')
+
