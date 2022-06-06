e52af0af5dadc879a10ee0cc9e4304dd9e231d47
AleDavMart <aleyda.davalos@gmail.com>
2022-06-05 22:30:06 -0700

    freeze model names to tables

--- server/models/db.js
+++ server/models/db.js
@@ -1,7 +1,18 @@
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const sequelize = new Sequelize('postgresql://localhost/PsychichGuacamole', {
+  define: {  //makes sure that sequelize does not pluralize the table name
+    freezeTableName: true
+  },
   logging: (...msg) => console.log(msg) //Displays all log function call parameters
 }); // passing a connection URI
+
+try { // checking the connection to the db
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+}catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
 
 class User extends Model {}
 
@@ -29,11 +40,5 @@ User.init({
 });
 
 
-try { // checking the connection to the db
-  sequelize.authenticate();
-  console.log('Connection has been established successfully.');
-} catch (error) {
-  console.error('Unable to connect to the database:', error);
-}
 
 
