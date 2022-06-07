613f9e6db959c10a1bf5e243a2ac2cbc83b69ca4
AleDavMart <aleyda.davalos@gmail.com>
2022-06-07 16:49:38 -0700

    Adding db associations

--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,7 @@ const { Sequelize, DataTypes, Model } = require('./db');
 const { User } = require('./User')
 const {Product} = require('./Product')
 const {Cart} = require('./Cart')
-const {Payment} = require ('./Payment')
+const {Payment} = require ('./Payment');
 
+User.hasMany(Payment) //can shop muitlple times at a store
+User.hasOne(Cart)
