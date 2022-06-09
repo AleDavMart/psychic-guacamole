1b587dfb8e887337a1137cc278e01c18d65a3597
AleDavMart <aleyda.davalos@gmail.com>
2022-06-09 15:43:26 -0700

    removed association since it is not needed

--- server/models/index.js
+++ server/models/index.js
@@ -6,4 +6,6 @@ const {Cart} = require('./Cart')
 const {Payment} = require ('./Payment');
 
 User.hasMany(Payment) //can shop muitlple times at a store
-User.hasOne(Cart)
+User.hasOne(Cart)// can only have on active shopping cart
+Cart.hasMany(Product) // can have multiple items in shopping cart
+
