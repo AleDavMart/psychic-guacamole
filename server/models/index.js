2720ce3ed89d9b409d41e36306363bc4aa67eeb5
AleDavMart <aleyda.davalos@gmail.com>
2022-06-20 17:53:17 -0700

    adjusted index.js to reflect db changes

--- server/models/index.js
+++ server/models/index.js
@@ -3,24 +3,51 @@ const { Sequelize, DataTypes, Model } = require('./db');
 const { User } = require('./User')
 const {Product} = require('./Product')
 const {Cart} = require('./Cart')
-const {Payment} = require ('./Payment');
+const {Payment} = require ('./Payment')
+const {Order} = require('./Order')
 
-User.hasMany(Payment, { //a shopper can shop muitlple times at a store
-  foreignKey:{
-    UserID: 'id' // this will tie the payments to a specific user
-  }
-}) 
+
 User.hasOne(Cart,{
-    foreignKey:{
-      userid: 'id'
-       // this will tie the open shopping cart to a user
-    }
-  })// can only have on active shopping cart
+  foreignKey: {userid : 'id'}
+});
+Cart.belongsTo(User);
 
 
-Cart.hasMany(Product, {
-  foreignKey:{
-    : 'id' // this will tie the payments to a specific user
+Product.belongsTo(Cart, {
+  foreignKey: {items: 'id'}
+});
+Cart.hasMany(Product);
+
+Cart.belongsTo(Payment, {
+  foreignKey : {
+    merchantReference: 'id',
+    amount: 'grandTotal'
   }
-}) // can have multiple items in shopping cart
+});
+Cart.hasOne(Payment); // is this needed as a prevention?
+
+Order.belongsTo(Payment, {
+  foreignKey: {
+    id: 'merchantReference', 
+    currency: 'currency',
+    orderGTotal: 'amount',
+    brand: 'brand',
+    paymentStatus: 'status'
+  }
+});
+
+
+
+
+
+// User.hasMany(Payment, { //a shopper can shop muitlple times at a store
+//   foreignKey:{
+//     UserID: 'id' // this will tie the payments to a specific user
+//   }
+// }) 
+
+
+
+// Cart.hasMany(Product, {
+// }) // can have multiple items in shopping cart
 
