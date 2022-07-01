e98b4915cf2a215566c05978224c4e52afbbd3c0
AleDavMart <aleyda.davalos@gmail.com>
2022-07-01 07:58:02 -0700

    First Iteration of seed function

--- server/seed.js
+++ server/seed.js
@@ -8,3 +8,27 @@ const {User} = require ('./models/index')
 const { Product} = require ('./models/index')
 
 //seed function
+const seed = async () =>{
+  const userBuffer = await fs.readFile(user_seedPath)
+  const {userdata} = JSON.parse(String(userBuffer))
+
+  const productBuffer = await fs.readFile(product_seedPath)
+  const {productdata} = JSON.parse(String(productBuffer))
+
+  //resolving the promises 
+  const userPromises = userdata.map( user => {
+      User.create(user)
+  })
+
+  const productPromises = productdata.map( product => { 
+      Product.create(product)
+  })
+
+  await Promise.all(userPromises)
+  await Promise.all(productPromises)
+
+  console.log('User and Motivation data succesfully populated!')
+
+}
+
+module.exports = seed; 
