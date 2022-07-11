cee633e3d1252b2a7d672e9342850e524c9f30b9
AleDavMart <aleyda.davalos@gmail.com>
2022-07-11 08:21:55 -0700

    Server file changes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
- 
+ // imports
+const express = require('express')
+const app = express()
+const PORT = 3000
+const cors = require('cors')
+
+const seed = require('./seed')
+const { db } = require('./db')
