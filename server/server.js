19b3e6983b08052f577c766d85e7603ef3b4b5cb
AleDavMart <aleyda.davalos@gmail.com>
2023-01-03 08:57:33 -0800

    Testing

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const {User, Product, Cart, Payment, Order}= require('./models/index');
 const path = require ('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const { userInfo } = require('os');
 
 app.use(cors());
 
@@ -37,7 +38,7 @@ app.listen( PORT, () => {
 // -------DB Internal API Routes ------ //
 
 //Getting all Users
-app.get('/users', async (req, res)=>{
+app.get('/user', async (req, res)=>{
   try{
     const allUsers = await User.findAll();
     res.json(allUsers);
@@ -47,3 +48,42 @@ app.get('/users', async (req, res)=>{
 }
 });
 
+//Get a specfic user by userName
+app.get ('/user/:userName', async (req, res) => {
+  let userName = req.params.userName;
+  let user = await User.findOne({ where: {userName: `${userName}`}});
+
+  //NEED TO FIND A SPOT TO USE THIS LOGIC - SHOULD NOT RETURN SENSITIVE DATA
+  // if(user.shoppingCart === null){
+  //   return "Shopping cart empty";
+  // }else{
+  //   return user.shoppingCart;
+  // };
+
+  // if (user.orderHistory=== null){
+  //   return "No Order History";
+  // }else{
+  //   return user.orderHis,tory;
+  // };
+  // return res.json([user.firstName,user.lastName, user.shoppingCart, user.orderHistory]);
+
+  return res.json(user);
+
+    
+});
+
+//Get a list of all products
+app.get('/allproducts', async (res, req)=>{
+  try{
+    const allproduct = await Product.findAll();
+    res.json(allproduct) //should return array of products
+
+  }catch(error){
+    console.error(error);
+  }
+
+});
+
+//Get a list of all products in stock 
+
+//
